58c9b6155a2019b74af8828cb2093d68dbf71a83
sff <shifangfang@qianmi.com>
2018-11-12 19:05:15 +0800

    test shell脚本

--- test.sh
+++ test.sh
@@ -8,8 +8,9 @@ test_time=`date +'%Y%m%d%H%M%S'`
 test_output_dir="${program_dir}/test-output/"
 mkdir -p $test_output_dir
 test_out_html=custom-test-report.html
-java -jar ${program_dir}/target/${appName}-${version}.jar
+#java -jar ${program_dir}/target/${appName}-${version}.jar
+java -jar ${program_dir}/target/${appName}-${version}.jar ${test_output_dir} > ${test_output_dir}/${test_time}.html
 echo "测试完毕"
-
-cd $program_dir/test-output/
-mv $test_out_html report-$test_time.html
+#
+#cd $program_dir/test-output/
+#mv $test_out_html report-$test_time.html
